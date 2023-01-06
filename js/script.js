1abb5a2fd8ccb7451d184c1d83c6cb725d608b43
hnejman <108231655+hnejman@users.noreply.github.com>
2023-01-06 18:02:29 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-fetch('https://www.wtc.wat.edu.pl/Plany/WTC20NI1S1.htm')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://www.wtc.wat.edu.pl/Plany/WTC20NI1S1.htm', true);
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Pobieramy HTML ze strony
+    const html = request.responseText;
+    // Tworzymy element div i ustawiamy mu HTML pobrany ze strony
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    // Szukamy tabelki na stronie
+    const table = div.querySelector('table');
+    // Jeśli tabelka została znaleziona
+    if (table) {
+      // Zamieniamy ją na div'y
+      const newDiv = document.createElement('div');
+      newDiv.innerHTML = table.innerHTML;
+      // Wstawiamy div'y zamiast tabelki
+      table.parentNode.replaceChild(newDiv, table);
+    }
+  } else {
+    console.error('Wystąpił błąd podczas pobierania strony');
+  }
+};
+
+request.onerror = function() {
+  console.error('Wystąpił błąd podczas pobierania strony');
+};
+
+request.send();
